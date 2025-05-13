4acf2a2338cd716a1b67809d371be43d3597c0bf
Bakamashine <surajaaminu09@gmail.com>
2025-05-13 22:13:31 +0300

    Накатил react-bootstrap

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,5 +1,3 @@
-import "./bootstrap";
-
 import { InertiaProgress } from "@inertiajs/progress";
 import { createInertiaApp } from "@inertiajs/react";
 import React from "react";
@@ -9,12 +7,11 @@ InertiaProgress.init();
 
 createInertiaApp({
     resolve: (name) => {
-
-        const pages: Record<string, { default: React.ComponentType<any> }> = import.meta.glob("./Pages/**/*.tsx", {
-        eager: true,
-        });
+        const pages: Record<string, { default: React.ComponentType<any> }> =
+            import.meta.glob("./Pages/**/*.tsx", {
+                eager: true,
+            });
         return pages[`./Pages/${name}.tsx`];
-
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @viteReactRefresh 
-    {{-- @vite(["resources/ts/app.tsx", "resources/css/main.css"]) --}}
-    @vite(["resources/ts/app.tsx", "resources/css/app.css"])
+    @vite(["resources/ts/app.tsx", "resources/css/app.css","resources/ts/bootstrap.js"])
     @inertiaHead
   </head>
   <body>
     @inertia
+  
+    {{-- <script src="{{ asset('/resources/ts/bootstrap.js') }}"></script> --}}
+    {{-- @vite('resources/ts/bootstrap.js') --}}
   </body>
 </html>
