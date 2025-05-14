a501f47f4ff82fd61274c5c2e1f62317422019dc
Bakamashine <surajaaminu09@gmail.com>
2025-05-15 00:48:04 +0300

    Сделал вывод ошибок валидации на странице авторизации

--- resources/ts/Pages/auth/login.tsx
+++ resources/ts/Pages/auth/login.tsx
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import Layout from "../Layout";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { FormDataVisitorHelpers } from "axios";
+import { router, usePage } from "@inertiajs/react";
+import { route } from "ziggy-js";
 
-function BasicExample() {
+interface LoginInterface {
+    email: string;
+    password: string;
+}
+
+function Auth() {
+    const {errors} = usePage<{errors: Error}>().props;
+
+    const [values, setValues] = useState<LoginInterface>({
+        email: "",
+        password: "",
+    });
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        const { id, value } = e.target;
+        setValues((prevValues) => ({
+            ...prevValues,
+            [id]: value,
+        }));
+    }
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        router.post("/auth", values as Record<string, any>);
+    }
+
     return (
         <Layout>
-            <Form className="m-3">
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Ваша почта</Form.Label>
-                    <Form.Control type="email" placeholder="Введите почту" />
-                </Form.Group>
+            <Form className="m-3 bg-form" onSubmit={handleSubmit}>
+                <h5 className="text-center">Авторизация</h5>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Пароль</Form.Label>
-                    <Form.Control type="password" placeholder="Введите пароль" />
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Ваша почта</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Введите почту"
+                        onChange={handleChange}
+                        value={values.email}
+                    />
                 </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Пароль</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Введите пароль"
+                        onChange={handleChange}
+                        value={values.password}
+                    />
+                </Form.Group>
+                <p className="red">{errors.email}</p>
                 <Button variant="primary" type="submit">
                     Авторизоваться
                 </Button>
@@ -24,4 +64,4 @@ function BasicExample() {
     );
 }
 
-export default BasicExample;
+export default Auth;
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @viteReactRefresh 
-    @vite(["resources/ts/app.tsx", "resources/css/app.css","resources/ts/bootstrap.js"])
-    @inertiaHead
-  </head>
-  <body>
-    @routes
-    @inertia
-  
-    {{-- <script src="{{ asset('/resources/ts/bootstrap.js') }}"></script> --}}
-    {{-- @vite('resources/ts/bootstrap.js') --}}
-  </body>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  @viteReactRefresh
+  @vite([
+    "resources/ts/app.tsx",
+    "resources/css/app.css",
+    "resources/css/main.css",
+    "resources/ts/bootstrap.js",
+])
+  @inertiaHead
+</head>
+
+<body>
+  @routes
+  @inertia
+</body>
+
 </html>
