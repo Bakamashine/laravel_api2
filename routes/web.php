6c7231fcdd10c0882f64fe6c3316bb50138d8f79
Bakamashine <surajaaminu09@gmail.com>
2025-05-14 22:46:18 +0300

    Сделал шаблон

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\RegisterController;
 
-Route::inertia("/", 'index');
-Route::inertia("/admin", 'admin');
+Route::inertia("/", 'index')->name("main");
+Route::inertia("/admin", 'admin')->name("admin");
 
 Route::post("/register", RegisterController::class);
 Route::post("/auth", AuthController::class);
