25cc13d3f94f7ed5b67c241630f5090b73189241
Bakamashine <surajaaminu09@gmail.com>
2025-05-18 22:29:07 +0300

    Поправил логику регистрации

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,6 +4,7 @@ namespace App\Actions\Fortify;
 
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Services\RegisterService;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
@@ -20,25 +21,29 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        $password_rules = array_merge(
-            (new UserRequest())->rules()['password'],
-            ['confirmed']
-        );
-        Validator::make($input, [
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique(User::class),
-            ],
-            // 'password' => $this->passwordRules(),
-            'password' => $password_rules
-        ])->validate();
+        // $password_rules = array_merge(
+        //     (new UserRequest())->rules()['password'],
+        //     ['confirmed']
+        // );
+        // Validator::make($input, [
+        //     'email' => [
+        //         'required',
+        //         'string',
+        //         'email',
+        //         'max:255',
+        //         Rule::unique(User::class),
+        //     ],
+        //     // 'password' => $this->passwordRules(),
+        //     'password' => $password_rules
+        // ])->validate();
+        
+        Validator::make($input, (new UserRequest())->rules());
+        
+        return RegisterService::store($input);
 
-        return User::create([
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+        // return User::create([
+        //     'email' => $input['email'],
+        //     'password' => Hash::make($input['password']),
+        // ]);
     }
 }
